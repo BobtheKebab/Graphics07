8b76b48d3e898125ef3648505dd315a27261fad2
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2018-03-26 15:34:56 -0400

    Stuff

--- draw.c
+++ draw.c
@@ -27,6 +27,11 @@ void add_polygon( struct matrix *polygons,
                   double x0, double y0, double z0, 
                   double x1, double y1, double z1, 
                   double x2, double y2, double z2 ) {
+
+  add_point(polygons, x0, y0, z0);
+  add_point(polygons, x1, y1, z1);
+  add_point(polygons, x2, y2, z2);
+  
 }
 
 /*======== void draw_polygons() ==========
@@ -39,6 +44,32 @@ void add_polygon( struct matrix *polygons,
   triangles
   ====================*/
 void draw_polygons( struct matrix *polygons, screen s, color c ) {
+
+  if ( polygons->lastcol < 3 ) {
+    printf("Need at least 3 points to draw a polygon!\n");
+    return;
+  }
+ 
+  int point;
+  for (point=0; point < points->lastcol-1; point+=3)
+    draw_line( points->m[0][point],
+               points->m[1][point],
+               points->m[0][point+1],
+               points->m[1][point+1],
+               s, c);
+  
+  draw_line( points->m[0][point],
+	     points->m[1][point],
+               points->m[0][point+2],
+               points->m[1][point+2],
+               s, c);
+
+  draw_line( points->m[0][point+1],
+               points->m[1][point+1],
+               points->m[0][point+2],
+               points->m[1][point+2],
+               s, c);
+  
 }
 
 
