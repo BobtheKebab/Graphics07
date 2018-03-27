979cfe242d93ab4e80caeb09d493f052030fba7b
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2018-03-27 15:36:13 -0400

    more stuff

--- draw.c
+++ draw.c
@@ -51,24 +51,25 @@ void draw_polygons( struct matrix *polygons, screen s, color c ) {
   }
  
   int point;
-  for (point=0; point < points->lastcol-1; point+=3)
-    draw_line( points->m[0][point],
-               points->m[1][point],
-               points->m[0][point+1],
-               points->m[1][point+1],
+  for (point=0; point < polygons->lastcol-1; point+=3) {
+    draw_line( polygons->m[0][point],
+               polygons->m[1][point],
+               polygons->m[0][point+1],
+               polygons->m[1][point+1],
                s, c);
   
-  draw_line( points->m[0][point],
-	     points->m[1][point],
-               points->m[0][point+2],
-               points->m[1][point+2],
+    draw_line( polygons->m[0][point],
+	       polygons->m[1][point],
+               polygons->m[0][point+2],
+               polygons->m[1][point+2],
                s, c);
-
-  draw_line( points->m[0][point+1],
-               points->m[1][point+1],
-               points->m[0][point+2],
-               points->m[1][point+2],
+    
+    draw_line( polygons->m[0][point+1],
+               polygons->m[1][point+1],
+               polygons->m[0][point+2],
+               polygons->m[1][point+2],
                s, c);
+  }
   
 }
 
